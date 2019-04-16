397338329698849ebd3530dda998db59640f4492
Jianing Zhang <jianingzhang082@gmail.com>
2019-04-15 20:58:14 -0700

    put a new to fix the bug

--- src/dancer.js
+++ src/dancer.js
@@ -1,25 +1,22 @@
-var Dancer = function (top, left, timeBetweenSteps) {
-
+var Dancer = function(top, left, timeBetweenSteps) {
   this.$node = $('<span class="dancer"></span>');
   this.top = top;
   this.left = left;
   this.timeBetweenSteps = timeBetweenSteps;
   this.step();
   this.setPosition(top, left);
-
-
 };
 
-
-Dancer.prototype.step = function () {
-
+Dancer.prototype.step = function() {
+  var that = this;
   setTimeout(this.step.bind(this), this.timeBetweenSteps);
+  console.log("are you kidding?");
 };
 
-Dancer.prototype.setPosition = function (top, left) {
+Dancer.prototype.setPosition = function(top, left) {
   var styleSettings = {
     top: top,
-    left: left
+    left: left,
   };
   this.$node.css(styleSettings);
-}
+};
